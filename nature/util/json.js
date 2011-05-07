c3892cf48cd60d8079283ab3351027c17c002835
unknown <Administrator@.(none)>
2011-05-07 13:31:00 +0800

    add JSON module

---
+++ nature/util/json.js
@@ -0,0 +1,41 @@
+/**
+ * @fileoverview JSON 模块
+ * @author nanzhi<nanzhienai@163.com>
+ */
+define(function() {
+	
+	/**
+	 * JSON 模块
+	 * @name json
+	 * @namespace
+	 */
+	return {
+
+		/**
+		 * @lends json
+		 * @static
+		 */
+
+		/**
+		 * 解析 json
+		 * @param { String } str JSON 字符串
+		 * @return { Object } 解析后的 JSON 对象
+		 */
+		parse: function(str) {
+
+
+		},
+
+		/**
+		 * 转换 json 为字符串
+		 * @param { Object } obj json 对象
+		 * @return { String } 转换后的字符串
+		 */
+		stringify: function(obj) {
+
+
+		}
+
+	};
+	
+});
